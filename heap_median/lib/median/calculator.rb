3fc624c7425e53ff82ffa3949f590237635975e4
Bogdan Agafonov <bogdan.agafonov@gmail.com>
2016-10-16 17:15:41 +0300

    superdebug

--- heap_median/lib/median/calculator.rb
+++ heap_median/lib/median/calculator.rb
@@ -1,4 +1,5 @@
 require_relative './heap'
+require "bigdecimal"
 
 module Median
   # calculates median
--- heap_median/spec/median/calculator_spec.rb
+++ heap_median/spec/median/calculator_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
+require 'pry'
 require 'median/calculator'
 
-describe Median::Calculator do
-  describe '#<<' do
-    let(:subject) { described_class.new([]) }
+def read_examples(input, expected, &block)
+  input = IO.readlines(input)[1..-1]
+  expected = IO.readlines(expected)
+  index = 0
+  result = input.each_with_object({}) do |value, acc|
+    input_row = value.chomp.strip
+    expected_row = expected[input.find_index(value)]
+    index += 1
+    begin
+      yield input_row, expected_row
+    rescue Exception
+      puts "FAILED at row ##{index}, input: #{input_row}"\
+           ", expectd: #{expected_row}"
+    end
+    acc[input_row] = expected_row
+  end
+  result
+end
 
+describe Median::Calculator do
+  let(:subject) { described_class.new([]) }
+  describe '#<<' do
     before do
       subject << 1
     end
@@ -18,6 +37,25 @@ describe Median::Calculator do
         subject << num
       end
       expect(subject.median).to eq 5.5
+    end
+  end
+  describe 'works on huge lists' do
+    # let(:examples2) { read_examples('./test_input2.txt', './test_output2.txt') }
+    it 'works' do
+      read_examples('./test_input', './test_output') do |k, v|
+        subject << k.to_i
+        begin
+          expect(subject.median).to eq(v.to_f)
+        rescue Exception => e
+          puts subject.median
+          puts (subject.heap.elements.sort == subject.heap.sorted)
+          raise e
+        end
+      end
+      # examples2.each do |k, v|
+      #   subject << k.to_i
+      #   expect(subject.median).to eq(v.to_f)
+      # end
     end
   end
 end
