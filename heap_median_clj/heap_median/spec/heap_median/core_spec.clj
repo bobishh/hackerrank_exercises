a1dff4ee6706be162480519e9ff017bd8dd3e451
Bogdan Agafonov <bogdan.agafonov@gmail.com>
2016-10-18 20:13:46 +0300

    moar clojure

--- heap_median_clj/heap_median/spec/heap_median/core_spec.clj
+++ heap_median_clj/heap_median/spec/heap_median/core_spec.clj
@@ -2,6 +2,64 @@
   (:require [speclj.core :refer :all]
             [heap-median.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "#left"
+  (it "returns left node index for index"
+    (should= 3 (left 1))))
+
+(describe "#right"
+  (it "returns right node index for index"
+    (should= 4 (right 1))))
+
+(describe "#parent"
+  (it "returns parent node index for index"
+    (should= 0 (parent 1))))
+
+(describe "swap"
+  (it "swaps elements in vector"
+    (should= [1 3 2] (swap [1 2 3] 2 1))
+    (should= [1 3 4 2] (swap [1 3 2 4] 2 3))
+    ))
+
+(describe "smaller-node"
+  (it "returns smaller node value"
+    (should= 6 (smaller-node [1 2 3 4 5 6 7] 2))
+    (should= 4 (smaller-node [1 2 3 4 5 6 7] 1))
+    (should= 2 (smaller-node [1 2 3 4 5 6 7] 0))
+    ))
+
+(describe "smaller"
+  (it "returns smaller node index"
+    (should= 5 (smaller [1 2 3 4 5 6 7] 2))
+    (should= 3 (smaller [1 2 3 4 5 6 7] 1))
+    (should= 1 (smaller [1 2 3 4 5 6 7] 0))))
+
+(describe "nice-parent?"
+  (it "returns true if parent smaller than any node"
+    (should= true (nice-parent? [1 2 3 4 5] 0))
+    )
+  (it "returns false if parent bigger than some node"
+    (should= false (nice-parent? [20 2 1 3 4] 0))
+    )
+  (it "works when parent has no node"
+    (should= true (nice-parent? [0] 0))
+    )
+  )
+
+(describe "safe-get"
+  (it "returns number if index is ok"
+    (should= 3 (safe-get [1 2 3] 2))
+    )
+  (it "returns nil if index out of bounds"
+    (should= nil (safe-get [0] 1))
+    )
+  )
+
+(describe "<<"
+  (it "adds number to heap"
+    (should= [1 3 2] (<< [2 3] 1))
+    ))
+
+(describe "poll"
+  (it "removes number from root and reorganizes heap"
+    (should= '(1 [2 3])  (poll [1 2 3]))
+    ))
--- heap_median_clj/heap_median/src/heap_median/core.clj
+++ heap_median_clj/heap_median/src/heap_median/core.clj
@@ -1,85 +1,88 @@
 (ns heap-median.core)
 
-(defn- parent [index]
-  (Math/abs (Math/ceil (/ (- index 2) 2))
-            )
+(defn parent [index]
+  (-> (- index 2)
+      (/ 2)
+      (Math/ceil)
+      (Math/abs)
+      (int)
+   )
   )
 
 (defn left [index]
-  (+ (* index 2) 1)
+  (-> (* index 2)
+      (+ 1)
+   )
   )
 
-(defn- right [index]
+(defn right [index]
   (+ (left index) 1)
   )
 
-(defn- swap [vector index1 index2]
+(defn swap [vector index1 index2]
   (assoc vector index2 (vector index1) index1 (vector index2)))
 
 
-(defn- safe-get [vector index]
-  (if (>= index (count vector))
+(defn safe-get [vector index]
+  (if (or (>= index (count vector)) (< index 0))
     nil
     (vector index)
     )
   )
 
-(defn nice-parent? [vector index]
-  (let [right (safe-get vector (right index))
-        left (safe-get vector (right index))
-        (not (> parent (smaller-val index)))
-    )
-  )
-
-(defn smaller-val [vector index]
+(defn smaller-node [vector index]
   (let [left-val (safe-get vector (left index))
         right-val (safe-get vector (right index))]
     (if (not (= right-val nil))
       (min right-val left-val)
+      left-val
       )
     )
   )
 
-(defn smaller [index]
-  (if (= (smaller-val vector index) (safe-get vector (right index)))
+(defn smaller [vector index]
+  (if (= (smaller-node vector index) (safe-get vector (right index)))
     (right index)
     (left index)
     )
   )
 
-(defprotocol Heap
-             (organize [vector])
-             (heapify-up [vector index])
-             (heapify-down [vector index])
-             (pop [vector])
-             (<< [vector element])
-             )
-
-(deftype MinHeap [vector]
-  Heap
-  (heapify-up [vector index]
-    (if (>= index (count vector))
-             vector
-             (when (not (nice-parent? vector index))
-               (swap vector index (parent index))
-               (heapify-up vector (parent index)))
-             )
+(defn nice-parent? [vector index]
+  (let [parent-value (safe-get vector index) smaller-value (smaller-node vector index)]
+    (or (nil? smaller-value) (< parent-value smaller-value))
     )
-  (heapify-down [vector index]
-    ((if (<= index 0)
-       vector
-       (if (not (nice_parent? vector index)
-                (swap vector index (smaller vector index))
-                (heapify-down vector )
-                )
-         )
-       ))
-    )
-  (organize [vector] (heapify-up (heaipify-down vector 0) (count vector)))
-  (<< [vector element] (let [vec (conj vector element)])
-      (heapify-down vec 0)
-      )
-  (pop [vector] (let [vhead (peek vector) (vec (pop vector))]
-                  (list vhead (heapify-up vec (- (count vec) 1)))
-                 ))
   )
+
+(defn heapify-up [vector index]
+  (let [parent-index (parent index)]
+    (if (<= parent-index 0)
+      vector
+      (if (not (nice-parent? vector index))
+        (heapify-up (swap vector index parent-index) parent-index)
+        (heapify-up vector parent-index)
+        )
+      )
+    )
+  )
+
+(defn heapify-down [vector index]
+  (if (> index (count vector))
+    vector
+    (let [smaller-index (smaller vector index)]
+      (if (not (nice-parent? vector index))
+        (heapify-down (swap vector index smaller-index) smaller-index)
+        (heapify-down vector smaller-index)
+        )
+      )
+    )
+  )
+
+(defn << [heap element]
+  (let [vec (conj heap element)]
+    (heapify-up vec (- (count vec) 1))
+    ))
+
+(defn poll [heap]
+  (let [swapped (swap heap 0 (- (count heap) 1)) vhead (peek swapped) rest (pop swapped)]
+    (list vhead (heapify-down rest 0))
+    ))
